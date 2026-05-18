ba88170755279fe161d0e9bcd9358cf6d7e45f9f
Wild_U <baffiwild@gmail.com>
2026-05-18 14:19:31 +0200

    Update bootstrap.js

--- wildu-media-suite/js/bootstrap.js
+++ wildu-media-suite/js/bootstrap.js
@@ -1,4 +1,4 @@
-const WILDU_MEDIA_BOOT_VERSION = '0.3.8';
+const WILDU_MEDIA_BOOT_VERSION = '0.3.9';
 
 // Import versionato della config pubblica.
 // Serve a evitare che il Service Worker o il browser tengano una config vecchia.
